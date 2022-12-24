d8b9ac04de4306121c693d6a67044210f5cbbfd4
Aakash Pandya <apandya@mobiquityinc.com>
2022-12-24 17:24:20 +0530

    Updated changes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,5 +6,5 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => res.status(200).send("Hello!"));
+app.get("/", (req, res) => res.status(200).send("Hello world456!"));
 exports.app = functions.https.onRequest(app);
