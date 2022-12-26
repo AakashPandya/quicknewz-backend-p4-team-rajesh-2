16cb6a1414068f50fdef65b7ece932c85f9b6514
Aakash Pandya <apandya@mobiquityinc.com>
2022-12-26 15:04:02 +0530

    changes

---
+++ functions/src/index.ts
@@ -0,0 +1,32 @@
+import * as functions from "firebase-functions";
+
+import express from "express";
+import headlinesRoutes from "./routes/headlinesRoutes";
+
+// import mongoose from "mongoose";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// mongoose
+//   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+//   .connect(process.env.MONGODB_URI!)
+//   .then(() => {
+//     try {
+//       app.use("/headlines", headlinesRoutes);
+//     } catch (ex) {
+//       console.log(ex);
+//     }
+//   })
+// eslint-disable-next-line max-len
+//   .catch((ex) => console.log("something went wrong while connecting db", ex));
+
+app.use("/headlines", headlinesRoutes);
+exports.app = functions
+  .runWith({
+    timeoutSeconds: 540,
+    memory: "1GB",
+  })
+  .https.onRequest(app);
--- functions/src/routes/headlinesRoutes.ts
+++ functions/src/routes/headlinesRoutes.ts
@@ -1,3 +1,5 @@
+import * as functions from "firebase-functions";
+
 import express, { Request, Response } from "express";
 
 import headlinesModel from "../models/headline";
@@ -6,17 +8,23 @@ import mongoose from "mongoose";
 const dbURL = "mongodb+srv://test12:test12@cluster0.adge8n4.mongodb.net/test";
 
 const connection = async () => {
+  functions.logger.log("Connection");
+
   try {
+    functions.logger.log("Try block");
     await mongoose.connect(dbURL);
   } catch (error) {
+    functions.logger.log("Error", error);
     console.log("Database connectivity error", error);
   }
 };
 const router = express.Router();
 
 router.route("/").get(async (req: Request, res: Response) => {
-  console.log("HELLO AAKASH");
+  functions.logger.log("I am here");
+
   await connection();
+
   const headlines = await headlinesModel.find(
     {
       title: new RegExp("", "i"),
