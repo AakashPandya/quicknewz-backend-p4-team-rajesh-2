b549c0f288a41e0cc7dde1a7e938e4b3e7d51c7b
Aakash Pandya <apandya@mobiquityinc.com>
2022-12-28 16:08:37 +0530

    fixes

--- functions/src/routes/headlinesRoutes.ts
+++ functions/src/routes/headlinesRoutes.ts
@@ -3,18 +3,27 @@
 import express, { Request, Response } from "express";
 
 import headlinesModel from "../models/headline";
+import mongoose from "mongoose";
 
 const router = express.Router();
 
 router.route("/").get(async (req: Request, res: Response) => {
-  const headlines = await headlinesModel.find(
-    {
-      title: new RegExp("", "i"),
-    },
-    { _id: 0 },
-    { createdAt: -1 }
-  );
-  res.send(headlines);
+  mongoose
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    .connect(process.env.MONGODB_URI!)
+    .then(async () => {
+      const headlines = await headlinesModel.find(
+        {
+          title: new RegExp("", "i"),
+        },
+        { _id: 0 },
+        { createdAt: -1 }
+      );
+      res.send(headlines);
+    })
+    .catch((ex) => {
+      console.log("something went wrong while connecting db", ex);
+    });
 });
 
 export default router;
